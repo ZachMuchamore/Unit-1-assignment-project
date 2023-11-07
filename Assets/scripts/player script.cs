5f24bfbc1aec8c29ee06e16e5687e9693965e17c
ZachMuchamore <541337@chrome.escg.ac.uk>
2023-11-07 16:12:16 +0000

    07/11/23 2

--- Assets/scripts/player script.cs
+++ Assets/scripts/player script.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public bool grounded;
+    HelperScript helper;
     Animator anim;
     Rigidbody2D rb;
     SpriteRenderer sr;
@@ -11,6 +14,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        helper = gameObject.AddComponent<HelperScript>();
         sr = GetComponent<SpriteRenderer>();
         print("start");
         rb = GetComponent<Rigidbody2D>();
@@ -22,9 +26,14 @@ public class NewBehaviourScript : MonoBehaviour
     {
         anim.SetBool("player run", false);
         anim.SetBool("player jump", false);
-        int speed = 10;
+        int speed = 2;
+        if (Input.GetKey("x"))
+        {
+            helper.FlipObject(true);
+        }
         if (Input.GetKey("a") == true) // move left
         {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
             anim.SetBool("player run", true);
             print("player pressed left");
             transform.position = new Vector2(transform.position.x - (speed * Time.deltaTime), transform.position.y);
@@ -32,6 +41,7 @@ public class NewBehaviourScript : MonoBehaviour
 
         if (Input.GetKey("d") == true) // move right 
         {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
             anim.SetBool("player run", true);
             print("player pressed right");
             transform.position = new Vector2(transform.position.x + (speed * Time.deltaTime), transform.position.y);
@@ -39,11 +49,22 @@ public class NewBehaviourScript : MonoBehaviour
         if (Input.GetKeyDown("space") == true)
         {
             anim.SetBool("player jump", true);
-            rb.AddForce(new Vector3(0, 1, 0), ForceMode2D.Impulse);
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode2D.Impulse);
         }
         if (Input.GetKeyDown("f") == true)
         {
             anim.SetTrigger("player attack 1");
+        }
+        grounded = helper.DoRayCollisionCheck();
+        if (Input.GetKeyDown("space") == true && grounded == true)
+        {
+            anim.SetBool("player jump", true);
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode2D.Impulse);
+        }
+
+        if (rb.velocity.y != 0)
+        {
+            anim.SetBool("player jump", true);
         }
     }
 }
